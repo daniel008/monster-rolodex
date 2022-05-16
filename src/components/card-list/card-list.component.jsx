23341d7855279a793abf872a638fcbd39c1f3871
DanielMa <mwldaniel_008@126.com>
2022-05-16 18:58:27 +0800

    convert App from class to functional component.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,14 @@
-import { Component } from 'react'
-import './card-list.styles.css'
 import Card from '../card/card.component'
+import './card-list.styles.css'
 
-class CardList extends Component {
-  render() {
-    console.log(this.props.monsters)
-    // console.log('render from CardList')
-    const { monsters } = this.props
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => (
-          <Card monster={monster} key={monster.id} />
-        ))}
-      </div>
-    )
-  }
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => (
+        <Card monster={monster} key={monster.id} />
+      ))}
+    </div>
+  )
 }
 
 export default CardList
